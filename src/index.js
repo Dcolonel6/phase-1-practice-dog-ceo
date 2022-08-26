e2956341fb9a18866aad0884f52e439ef19d6847
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-26 15:11:12 +0300

    added the filter functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-'use strict';
+"use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
   const fetchImages = fetchDogs();
-  const dogBreeds = fetchDogs("https://dog.ceo/api/breeds/list/all");
-  
+  const dogBreeds = fetchDogs("https://dog.ceo/api/breeds/list/all"); 
   dogImages(fetchImages);
-  dogTypes(dogBreeds)
-  
+  dogTypes(dogBreeds);
+
 });
 
 function fetchDogs(url = "https://dog.ceo/api/breeds/image/random/4") {
@@ -24,44 +23,75 @@ function dogImages(promise) {
     })
     .catch((error) => console.error(error.message));
 }
-function liHandler(event){
-  this.hasAttribute('style') ? this.removeAttribute('style') : this.setAttribute('style', 'color: #565857;')   
-    
+function liHandler(event) {
+  this.hasAttribute("style")
+    ? this.removeAttribute("style")
+    : this.setAttribute("style", "color: #565857;");
 }
 
 function dogTypes(promise) {
-  promise
-  .then(({message}) => {
-    displayListOfDogs(message)
-    const allListItems = document.querySelectorAll('li')   
-    Array.from(allListItems).forEach(li => li.addEventListener('click', liHandler))
-
+  promise.then(({ message }) => {
+    displayListOfDogs(message);
+    const allListItems = document.querySelectorAll("li");
+    const select = document.querySelector('select')
+    Array.from(allListItems).forEach((li) =>
+      li.addEventListener("click", liHandler)
+    );
+    select.addEventListener('change',onchangeHandler.bind(message))
   });
 }
-function displayListOfDogs(dogsObj){
-    const ulDogBreeds = document.querySelector('#dog-breeds')
-    
-    for(const breed in dogsObj){
-        const li = createElement('li')
-        li.textContent = breed
-        if(dogsObj[breed].length ){
-            const ulNested = createElement('ul')
-            for(const subreed of dogsObj[breed]){
-                const liNested = createElement('li')
-                liNested.textContent = subreed
-                ulNested.append(liNested)
-            }
-            li.append(ulNested)
-        }else{
-            continue
-        }
-        ulDogBreeds.append(li)
+function displayListOfDogs(dogsObj) {
+  const ulDogBreeds = document.querySelector("#dog-breeds");
+  ulDogBreeds.textContent = ''
 
+  for (const breed in dogsObj) {   
+    
+    if (dogsObj[breed].length) {      
+      for (const subreed of dogsObj[breed]) {
+        const li = createElement("li");       
+        li.textContent = `${breed} - ${subreed}`;
+        ulDogBreeds.append(li);
+      }      
+      
+    } else {
+      const li = createElement("li");       
+        li.textContent = `${breed} `;
+        ulDogBreeds.append(li);
     }
     
+  }
+}
 
+function onchangeHandler(event){
+  const { target } = event
+  const filteredDogs = filterDog(this,target.value)
+  console.log(filteredDogs)
+  displayListOfDogs(filteredDogs)
+}
+
+function filterDog(dogObj, q=''){
+  //console.log(dogObj)
+  if(q){
+    //use query to filter and return the results
+    const newObj = {}
+    const dogStartWithQuery = Object.keys(dogObj).filter(dog => dog.toLowerCase().startsWith(q.toLowerCase()))
+    for(const dog of dogStartWithQuery){
+      
+      if(Array.isArray(dogObj[dog])){
+        const listSubbreed = [...dogObj[dog]]
+        newObj[dog] = listSubbreed
+      }
+      else{
+        newObj[dog] = []
+      }
+    }
+    return newObj  
+
+  }
+  return dogObj
 
 }
+
 function createElement(tag, attributes = {}) {
   const element = document.createElement(tag);
   if (Object.keys(attributes).length > 0) {
